731e0cb227406541ca7d1fbec741233ee8aaf403
mcakunz <matheus.kunz05@gmail.com>
2025-09-04 21:35:55 -0300

    primeiro commit

---
+++ VisitasExternas/VisitasExternas/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "VisitasExternas")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "VisitasExternas.Pages")]
---
+++ VisitasExternas/VisitasExternas/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VisitasExternas
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
